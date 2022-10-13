98d7e0e036362dc9c9f849ca508c8716a490674a
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-10-13 19:12:39 +0900

    Update `test\W3Resource` add tests
    Tests added:
    - [x] NumberThree
    - [x] NumberFour
    - [x] NumberFive
    - [x] NumberSix

--- test/tests/W3Resource.purs
+++ test/tests/W3Resource.purs
@@ -2,12 +2,19 @@ module W3Resource where
 
 import Prelude
 
+import NumberFive (testNumberFive)
+import NumberFour (testNumberFour)
 import NumberOne (testNumberOne)
+import NumberSix (testNumberSix)
+import NumberThree (testNumberThree)
 import NumberTwo (testNumberTwo)
 import Test.Spec (Spec)
-
 
 w3Resource ∷ Spec Unit
 w3Resource = do
   testNumberOne
   testNumberTwo
+  testNumberThree
+  testNumberFour
+  testNumberFive
+  testNumberSix
